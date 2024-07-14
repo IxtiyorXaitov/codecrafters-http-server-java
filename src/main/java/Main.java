604b4351dccd84f68f2f8a8df22eb3c62b0570c7
IxtiyorXaitov <ikhtiyordev@gmail.com>
2024-07-14 11:03:38 +0500

    Extract URL path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,50 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        System.out.println("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
 
         try {
             serverSocket = new ServerSocket(4221);
-            // Since the tester restarts your program quite often, setting SO_REUSEADDR
-            // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
 
-            clientSocket = serverSocket.accept(); // Wait for connection from client.
+            clientSocket = serverSocket.accept();
+
+            InputStream inputStream = clientSocket.getInputStream();
+
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(
+                            inputStream, StandardCharsets.UTF_8
+                    )
+            );
+            String readLine = bufferedReader.readLine();
+            String[] strings = readLine.split(" ");
+
+            String method = strings[0];
+            String path = strings[1];
+            String httpVersion = strings[1];
+
+            System.out.println("method -> " + method);
+            System.out.println("path -> " + path);
+            System.out.println("httpVersion -> " + httpVersion);
+
+            String response = "HTTP/1.1 404 NOTFOUND\r\n\r\n";
+
+            if (Objects.equals(path, "/")) {
+                response = "HTTP/1.1 200 OK\r\n\r\n";
+            }
 
             clientSocket
                     .getOutputStream()
-                    .write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+                    .write(response.getBytes(StandardCharsets.UTF_8));
 
             clientSocket.close();
             System.out.println("accepted new connection");
